8ae82707ae6614f3f6d88674521fd895b0de642f
ALMufrashA <mufrashajmal2001@gmail.com>
2025-07-26 11:25:56 +0530

    Utils Page

---
+++ utils.py
@@ -0,0 +1,21 @@
+
+import base64
+import streamlit as st
+
+def set_background(image_path):
+    image_path = "C:/Users/HP/OneDrive/Desktop/Sentence Emotion/background_home.jpeg"
+    with open(image_path, "rb") as image_file:
+        encoded = base64.b64encode(image_file.read()).decode()
+    css = f"""
+    <style>
+    .stApp {{
+        background-image: url("data:image/jpeg;base64,{encoded}");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    </style>
+    """
+    st.markdown(css, unsafe_allow_html=True)
+
+
